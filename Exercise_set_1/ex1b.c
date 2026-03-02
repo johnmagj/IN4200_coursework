f0790d5c6a3a7dad9c6b15b50b0183d035893c3d
John-Magnus Johnsen <jom_jon@hotmail.com>
2026-03-02 11:18:54 +0100

    Completed

--- Exercise_set_1/ex1b.c
+++ Exercise_set_1/ex1b.c
@@ -14,14 +14,39 @@ int main() {
 
     int arr[n];
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
 
         arr[i] = rand();
 
         // Print the array element immediately
         printf("a[%d] = %d\n", i, arr[i]);
-
     }
+
+    int elem_min = arr[0];
+    int elem_max = arr[0];
+
+    int elem_min_indx = 0;
+    int elem_max_indx = 0;
+
+    for (int i = 1; i < n; i++) {
+
+        if (arr[i] < elem_min) {
+
+            elem_min = arr[i];
+            elem_min_indx = i;
+        }
     
+        if (arr[i] > elem_max) {
+
+            elem_max = arr[i];
+            elem_max_indx = i;
+        }
+    }
+
+    printf("Minimum value: %i, at index: %i \n", elem_min, elem_min_indx);
+    printf("Maximum value: %i, at index: %i", elem_max, elem_max_indx);
+
     return 0;
 }
+
+// Done
--- Exercise_set_1/ex1c.c
+++ Exercise_set_1/ex1c.c
@@ -4,48 +4,52 @@
 
 int main() {
 
-    int m = 3;
-    int n = 3;
+    int m = 10000;
+    int n = 10000;
+
+    int **arr1 = malloc(m*sizeof(*arr1));
+    int **arr2 = malloc(m*sizeof(*arr2));
+
+    for (int i = 0; i < m; i++) {
+        arr1[i] = malloc(n * sizeof(*arr1));
+        arr2[i] = malloc(n * sizeof(*arr2));
+    }
 
     clock_t t_start, t_stop;
     double t_tot;
+
     t_start = clock();
-
-    int matrix_1[m][n];
-
     for(int i = 0; i < m; i++) {
         for(int j = 0; j < n; j++) {
 
-            matrix_1[i][j] = 1;
+            arr1[i][j] = 1;
 
-            printf("%p ", &matrix_1[i][j]);
+            // printf("%p ", &matrix_1[i][j]);
         }
-        printf("\n");
+        // printf("\n");
     }
-
     t_stop = clock();
-    t_tot = (double)(t_stop - t_start);
-
+    t_tot = (double)(t_stop - t_start)/CLOCKS_PER_SEC;
     printf("%f\n", t_tot);
 
     t_start = clock();
-
-    int matrix_2[m][n];
-
     for(int j = 0; j < m; j++) {
         for(int i = 0; i < n; i++) {
 
-            matrix_2[i][j] = 1;
+            arr2[i][j] = 1;
 
-            printf("%p ", &matrix_2[i][j]);
+            // printf("%p ", &matrix_2[i][j]);
         }
-        printf("\n");
+        // printf("\n");
     }
-
     t_stop = clock();
-    t_tot = (double)(t_stop - t_start);
-    
-    printf("%f", t_tot);
+    t_tot = (double)(t_stop - t_start)/CLOCKS_PER_SEC;
+    printf("%f\n", t_tot);
+
+    printf("%d\n", arr1[0][0]);
+    printf("%d\n", arr2[0][0]);
 
     return 0;
 }
+
+// Done
