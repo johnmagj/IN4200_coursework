07439367a83f45841af67b643e600579c7865592
John-Magnus Johnsen <jom_jon@hotmail.com>
2026-03-25 09:30:41 +0100

    read_s.. now runs as intended from main

--- Mandatory_assignment1/main.c
+++ Mandatory_assignment1/main.c
@@ -5,11 +5,28 @@
 int main(int nargs, char **args) {
 
     char filename_path[256];
-
+    //char filename_path[] = "Data/1138_bus.mtx";
+    double **A, **B;
+    struct sparse_mat_coo S_coo, C_coo;
+    //struct spare_mat_crs S_crs, C_crs;
+    
     printf("Enter relative file path to the .mtx file:");
     scanf("%s", &filename_path);
     
-    printf("%s", filename_path);
+    // printf("%s", filename_path);
+
+    read_sparse_matrix_from_file(filename_path, &S_coo);
+
+    printf("--------------------------------\n");
+    printf("For file: %s\n", filename_path);
+    printf("n: %d, nnz: %d\n", S_coo.n, S_coo.nnz);
+    printf("First: %d, %d, %f\n", S_coo.row_idx[0], S_coo.col_idx[0], S_coo.val[0]);
+    printf("Last: %d, %d, %f\n", S_coo.row_idx[S_coo.nnz-1], S_coo.col_idx[S_coo.nnz-1], S_coo.val[S_coo.nnz-1]);
+    printf("--------------------------------");
+
+    free(S_coo.row_idx);
+    free(S_coo.col_idx);
+    free(S_coo.val);
 
     return 0;
 }
--- Mandatory_assignment1/read_sparse_matrix_from_file.c
+++ Mandatory_assignment1/read_sparse_matrix_from_file.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "function_declarations.h"
 
 void read_sparse_matrix_from_file(const char *filename, struct sparse_mat_coo *matrix) {
 
@@ -6,27 +8,25 @@ void read_sparse_matrix_from_file(const char *filename, struct sparse_mat_coo *m
     FILE *file;
 
     // Opening file in read mode ("r")
-    file = fopen("Data/psmigr_1.mtx", "r");
+    file = fopen(filename, "r");
 
     if (file == NULL) {
         printf("Error in opening file");
-        return 1;
+        return;
     }
+
+    int m_rows, n_cols, total_values;
 
     const int LINE_BUFFER = 256;
     char line_in_file[LINE_BUFFER];
-
-    int m, n, total_values;
-
-    // 
+    // Scan through file, break after collecting matrix info on the first line after the comments in file
     while (fgets(line_in_file, sizeof(line_in_file), file) != NULL) {
         
         if (line_in_file[0] == '%') {
             /* Pass */;
         }
         else {
-            // printf("%s", line_buffer);
-            sscanf(line_in_file, "%d %d %d", &m, &n, &total_values);
+            sscanf(line_in_file, "%d %d %d", &m_rows, &n_cols, &total_values);
             break;
         }
     }
@@ -34,15 +34,10 @@ void read_sparse_matrix_from_file(const char *filename, struct sparse_mat_coo *m
     int *rows = malloc(total_values*sizeof(*rows));
     int *cols = malloc(total_values*sizeof(*cols));
     double *values = malloc(total_values*sizeof(*values));
-
-    // fgets will continue on the next line in file from where it left off above 
+    // fgets will continue on the next line in file from where it left off above, first data point
     // Index first row of values as 0
     int i = 0;
     while (fgets(line_in_file, sizeof(line_in_file), file) != NULL) {
-
-        if (i%100000 == 0) {
-            printf("%s", line_in_file);
-        }
 
         sscanf(line_in_file, "%d %d %lf", &rows[i], &cols[i], &values[i]);
         i += 1;
@@ -50,17 +45,16 @@ void read_sparse_matrix_from_file(const char *filename, struct sparse_mat_coo *m
     
     fclose(file);
 
-    printf("i = %d\n", i);
-
-    printf("num header lines: %d\n", num_header_lines);
-    printf("m: %d, n: %d, tot values: %d\n", m, n, total_values);
-
-    printf("%d, %d, %f\n", rows[0], cols[0], values[0]);
-    printf("%d, %d, %f\n", rows[total_values-1], cols[total_values-1], values[total_values-1]);
-
-    free(rows);
-    free(cols);
-    free(values);
-
+    // Asigning matrix values to struct
+    // We just pick n to be the number of rows in the data matrix
+    matrix->n = m_rows;
+    matrix->nnz = total_values;
+    matrix->row_idx = rows;
+    matrix->col_idx = cols;
+    matrix->val = values;
+    
+    // free(rows);
+    // free(cols);
+    // free(values);
 }
 
