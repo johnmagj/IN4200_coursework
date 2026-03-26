406308870e59f65bc80e5be49de4419e77ab036b
John-Magnus Johnsen <jom_jon@hotmail.com>
2026-03-26 14:03:40 +0100

    Add folder and file

---
+++ Exercise_set_8/ex2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int arr[] = {3, 2, 3, 8, 5, 6, 4, 1};
+
+    int n = 8;
+
+    for (int i = 0; i < n - 1; i++) {
+        if (i%2 == 0) {
+
+            for (int j = 0; j < )
+        }
+
+    }
+
+    return 0;
+}
