6b8c31295cc4f19579ba624740821b630a973d82
John-Magnus Johnsen <jom_jon@hotmail.com>
2026-04-07 13:19:52 +0200

    Add some CRS multiplication code, working

--- Mandatory_assignment1/sampled_matrix_multiplication_crs.c
+++ Mandatory_assignment1/sampled_matrix_multiplication_crs.c
@@ -7,29 +7,28 @@ void sampled_matrix_multiplication_crs(struct sparse_mat_crs *C, double **A, dou
     int n = S->n;
     int nnz = S->nnz;
    
-    int current_row = 1;
-    int current_val_index = -1;
+    int current_row = 0;
+    int current_val_index = 0;
     
     for (int index = 0; index < n+1; index++) {
 
-        if (S->row_ptr[index] > current_val_index) {
-        
-            int i = current_row - 1;
-            int j = S->col_idx[index] - 1;
+        if (S->row_ptr[index+1] > current_row) {
+            
+            current_row = index;
 
-            double sum = 0;
-            for (int k = 0; k < n; k++) {
-                sum += A[i][k]*B[k][j];
+            int i = current_row;
+            printf("here\n");
+            for (int col_index = 0; col_index < nnz; col_index++) {
+
+                int j = S->col_idx[col_index];
+
+                double sum = 0;
+                for (int k = 0; k < n; k++) {
+                    sum += A[i][k]*B[k][j];
+                }
+
+                C->val[index] = sum*S->val[index];
             }
-
-            C->val[index] = sum*S->val[index];
-
-            current_row += 1;
-            current_val_index += 1;
-        }
-
-        else {
-
         }
     }
 
