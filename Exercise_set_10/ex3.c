3d02bb7ae4b7d42774fa9e4d4db56d1584bec8c5
John-Magnus Johnsen <jom_jon@hotmail.com>
2026-04-26 14:31:25 +0200

    Complete exercise

--- Exercise_set_10/ex3.c
+++ Exercise_set_10/ex3.c
@@ -17,79 +17,92 @@ int main(int nargs, char **args) {
 
     int N = 10;
 
-    int *sendcounts = NULL;
-    int *displacements = NULL;
+    int min_rows_per_rank = N/size; // integer division since both numerator and denominator are int
+    int reminder = N % size;
+    
+    int *rows_per_rank = malloc(size*sizeof(*rows_per_rank));;
 
-    // Logic for deciding how many rows each process gets
-    if (N < size) {
-        sendcounts = calloc(size, sizeof(*sendcounts));
-        sendcounts[0] = {N};
+    // For MPI_Scatterv
+    int *send_counts = malloc(size*sizeof(*send_counts));
+    int *send_displacements = malloc(size*sizeof(*send_displacements));
+    // For MPI_Gatherv
+    int *recv_displacements = malloc(size*sizeof(*recv_displacements));
 
-        displacements = calloc(size, sizeof(*sendcounts));
-    } 
-    else if (N % size == 0) {
-        sendcounts = malloc(size*sizeof(*sendcounts));
-        int rows_per_rank = N/size;
-        for (int i = 0; i < size; i++) {
-            sendcounts[i] = rows_per_rank;
-        }
-        displacements = malloc(size*sizeof(*sendcounts));
-        for (int i = 0; i < size; i++) {
-            displacements[i] = i*rows_per_rank;
-        }
-    }
-    else if (N % size != 0) {
-        sendcounts = malloc(size*sizeof(*sendcounts));
-        int rows_per_rank = (N - (N % size))/size;
-        for (int i = 0; i < size-1; i++) {
-            sendcounts[i] = rows_per_rank;
-        }
-        sendcounts[size-1] = rows_per_rank + 
+    int threshold = size - reminder;
+    int row_offset = 0;
+    int y_offset = 0;
+    for (int i = 0; i < size; i++) {
+        int number_of_rows = min_rows_per_rank + (i >= threshold);
+
+        rows_per_rank[i] = number_of_rows;
+        send_counts[i] = number_of_rows*N;
+        send_displacements[i] = row_offset;
+        row_offset += send_counts[i];
+
+        recv_displacements[i] = y_offset;
+        y_offset += number_of_rows;
     }
 
-    double *A_row_recv = malloc(N*sizeof(*A_row_recv));
-
-    // So we can use MPI_Bcast later
-    double *x = NULL;
-    double *y = NULL; 
+    # define idx(i, j) (i*N + j)
+    double *A = NULL;
+    double *x = malloc(N*sizeof(*x));
 
     if (rank == root_rank) {
-
-        # define idx(i, j) (i*N + j)
-        
-        double *A = malloc(N*N*sizeof(*A));
-        x = malloc(N*sizeof(*A));
-
+        A = malloc(N*N*sizeof(*A));
         // Fill matrix A and vector x with ones
         for (int i = 0; i < N; i++) {
             x[i] = 1.0;
-            for (int j = 0; j < N, j++) {
+            for (int j = 0; j < N; j++) {
                 A[idx(i, j)] = 1.0;
             }
         }
-
-    MPI_Scatter(&A[idx(row_idx, 0)], N, MPI_DOUBLE, &A_row_recv, MPI_DOUBLE, root_rank, MPI_COMM_WORLD);
-    MPI_Bcast(x, 1, MPI_DOUBLE, root_rank, MPI_COMM_WORLD);
-    
-    free(A);
     }
-
     else {
-        Receive(A[idx(i, j)])
-        Receive(x[i])
-
-        int row_sum = 0; 
-        for (int elem = 0; elem < N; elem++) {
-            row_sum += A[idx(elem, j)]*x[elem]
-        }
-
-        y[elem] = row_sum
-
-        Send(y)
+        A = malloc(rows_per_rank[rank]*N*sizeof(*A));
     }
 
+    double *A_recv = malloc(rows_per_rank[rank]*N*sizeof(*A_recv));
+
+    MPI_Bcast(x, N, MPI_DOUBLE, root_rank, MPI_COMM_WORLD);
+    MPI_Scatterv(A, send_counts, send_displacements, MPI_DOUBLE, A_recv, send_counts[rank], MPI_DOUBLE, root_rank, MPI_COMM_WORLD);
+
+    double *y = NULL;
+    double *y_recv = NULL;
+    if (rank == root_rank) {
+        y = malloc(N*sizeof(*y));
+        y_recv = malloc(N*sizeof(*y_recv));
+    }
+    else {
+        y = malloc(rows_per_rank[rank]*sizeof(*y));
+        y_recv = malloc(rows_per_rank[rank]*sizeof(*y_recv));
+    }
+
+    // The actual matrix multiplication
+    for (int i = 0; i < rows_per_rank[rank]; i++) {
+        y[i] = 0;
+        for (int j = 0; j < N; j++) {
+            y[i] += A_recv[idx(i, j)]*x[j];
+        }
+    }
+
+    MPI_Gatherv(y, rows_per_rank[rank], MPI_DOUBLE, y_recv, rows_per_rank, recv_displacements, MPI_DOUBLE, root_rank, MPI_COMM_WORLD);
+
+    if (rank == root_rank) {
+
+        for (int i = 0; i < N; i++) {
+            printf("%d, %f\n", i, y_recv[i]);
+        }
+    }
+
+    free(rows_per_rank);
+    free(A);
+    free(A_recv);
     free(x);
     free(y);
+    free(y_recv);
+    free(send_counts);
+    free(send_displacements);
+    free(recv_displacements);
 
     MPI_Finalize();
 
